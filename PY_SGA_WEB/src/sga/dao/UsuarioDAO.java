533d2bc18db5318090f400ff00964c10a8aae5e7
Edwin0129 <edwin.v.cuenca@gmail.com>
2016-02-02 01:03:49 -0500

    5

--- PY_SGA_WEB/src/sga/dao/UsuarioDAO.java
+++ PY_SGA_WEB/src/sga/dao/UsuarioDAO.java
@@ -1,10 +1,28 @@
 package sga.dao;
 
+import java.io.Reader;
+
 import sga.beans.Usuario;
 import sga.service.UsuarioService;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
 public class UsuarioDAO implements UsuarioService {
 
+	SqlSessionFactory sqlmapper=null;{
+		try {
+			Reader archi=
+				Resources.getResourceAsReader("ConfiguracionIbatis.xml");	
+			sqlmapper= new SqlSessionFactoryBuilder().build(archi);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
 	@Override
 	public Usuario USPSEGUSUARIO(Usuario objUsuario) {
 		// TODO Auto-generated method stub
